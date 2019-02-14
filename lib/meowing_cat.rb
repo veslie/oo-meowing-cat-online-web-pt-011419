ef789db34532a85ac7cd843cb044fe518f635529
veslie losius <veslielosius@yahoo.com>
2019-02-14 08:40:30 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+class Cat 
+  class Person
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+end
